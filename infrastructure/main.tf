36238dfc186f45ed3a5062dd836904bae4c0118a
amr205 <alejandromedinareyesmx@gmail.com>
2021-09-01 16:23:55 -0600

    proyecto inicial

---
+++ infrastructure/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  backend "remote" {
+    organization = "amr205"
+
+    workspaces {
+      name = "AzureWagtail"
+    }
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "azureWagtail" {
+  name     = "azureWagtailResourceGroup"
+  location = "westus"
+}
+
+resource "azurerm_app_service_plan" "azureWagtail" {
+  name                = "slotAppServicePlan"
+  location            = azurerm_resource_group.azureWagtail.location
+  resource_group_name = azurerm_resource_group.azureWagtail.name
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "azureWagtail" {
+  name                = "doaAzWebAppAlex"
+  location            = azurerm_resource_group.azureWagtail.location
+  resource_group_name = azurerm_resource_group.azureWagtail.name
+  app_service_plan_id = azurerm_app_service_plan.azureWagtail.id
+
+  site_config {
+    python_version = "3.4"
+  }
+}
+
+resource "azurerm_app_service_slot" "azureWagtail" {
+  name                = "doaAzWebAppAlexSlotOne"
+  location            = azurerm_resource_group.azureWagtail.location
+  resource_group_name = azurerm_resource_group.azureWagtail.name
+  app_service_plan_id = azurerm_app_service_plan.azureWagtail.id
+  app_service_name    = azurerm_app_service.azureWagtail.name
+}
